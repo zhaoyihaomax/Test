ae5f9fcf729c1163571206900cd72d1f929adee3
zhaoyihaomax <55304983+zhaoyihaomax@users.noreply.github.com>
2019-10-26 10:38:54 +0800

    1026

---
+++ 1026.cpp
@@ -0,0 +1,89 @@
+//#include<iostream>  //1.0 ’€∞Î 
+//using namespace std;
+//int main()
+//{
+//	int a[11000]={-1,1,2,3,4,6,101,105,111,170};
+//	int x = 150;
+//	int left=1,right=9,mid;
+//	while(left<=right)
+//	{
+//		mid=(left+right)/2;
+//		if(a[mid]<=x)left=mid+1;
+//		else right =mid-1;
+//	}
+//	cout<<a[right]<<endl;
+//	return 0;
+//}
+
+
+
+//#include<iostream>
+//#include<stdio.h> //2.0√∞≈›≈≈–Ú  
+//using namespace std;
+//int a[8]={6,10,11,8,4,1,9,7};
+//
+//void swap(int &a,int &b)
+//{
+//	int temp=a;
+//	a=b;
+//	b=temp;
+//}
+//
+//int main() 
+//{
+//	for(int j=7;j>0;j--)
+//	{
+//		for(int i=0;i<j;i++)
+//		{
+//			if(a[i]>a[i+1])swap(a[i],a[i+1]);
+//		}
+//	}
+//	for(int i=0;i<=7;i++)cout<<a[i]<<" ";
+//	return 0;
+//}
+
+
+
+
+
+//#include<iostream>
+//using namespace std;
+//int a[8]={6,10,11,8,4,1,9,7};
+//void swap(int &a,int &b)
+//{
+//	int temp=a;
+//	a=b;
+//	b=temp;	
+//}
+//int main()
+//{
+//	for(int j=0;j<7;j++)
+//	{
+//		for(int i=7;i<j;i++)
+//		{
+//			if(a[i]>a[i-1])swap(a[i],a[i+1]);
+//		}
+//	}
+//	for(int i=0;i<=7;i++)cout<<a[i]<<" ";
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
